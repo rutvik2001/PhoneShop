96400aa9d84e7e05717956a71ea1bee1442af12d
Rutvikkumar Narendrakumar <P2516414@my365.dmu.ac.uk>
2021-02-15 14:55:07 +0000

    working on testing complaint

---
+++ Testing2/tstComplaints.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstComplaints
+    {
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsAddress AnAddress = new clsAddress();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnAddress.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnAddress.Active, TestData);
+        }
+
+    }
+}
